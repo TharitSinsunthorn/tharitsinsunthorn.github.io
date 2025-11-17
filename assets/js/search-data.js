ce4848cca86f00faa0ab383dc0485e4b7a5098b2
TharitSinsunthorn <93856100+TharitSinsunthorn@users.noreply.github.com>
2025-11-17 11:59:08 +0000

    Deploying to gh-pages from @ TharitSinsunthorn/tharitsinsunthorn.github.io@241b9baf4961951223427b7b8295316bd5a280e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,18 +426,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-tharit-s-master-thesis-midterm-defense",
+          title: 'Tharit’s Master thesis midterm-defense.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {

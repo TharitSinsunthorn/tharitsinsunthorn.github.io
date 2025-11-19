73a7a4f1865a8f6f8057ee7e4d5b6890f3f909a0
TharitSinsunthorn <93856100+TharitSinsunthorn@users.noreply.github.com>
2025-11-19 11:53:19 +0000

    Deploying to gh-pages from @ TharitSinsunthorn/tharitsinsunthorn.github.io@7d75ac395850e8f0345fb0e074d63b046fa39b97 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "The below repositories include both my own projects and the projects that include my engagement.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";

8ecc01df8b22145b4fcb05835b59e333f3cfa6ba
TharitSinsunthorn <93856100+TharitSinsunthorn@users.noreply.github.com>
2025-11-27 06:09:57 +0000

    Deploying to gh-pages from @ TharitSinsunthorn/tharitsinsunthorn.github.io@fa0c0121bb128e6a3d112eee6644088bb4dbd104 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,6 +396,11 @@ ninja.data = [{
           description: "A digital twin of the JEM Internal Ball 2 in Isaac Sim.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/intball2_sim/";
+            },},{id: "projects-dymon-yaoki-mission-simulation",
+          title: 'Dymon-Yaoki mission simulation',
+          description: ".",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/jaops/";
             },},{id: "projects-rl-locomotion",
           title: 'RL Locomotion',
           description: "Locomotion policy for quadruped robot in low-gravity env.",
